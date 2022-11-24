4c3e9ca2f7c80b5f1bf26837b94025ff4ce4a2aa
Nidal-h <58577495+Nidal-h@users.noreply.github.com>
2022-11-24 09:34:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,10 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:4200",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
