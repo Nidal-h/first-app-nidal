331bdc175f7fe6ea7c06b8a8d8e619b0ed8b1354
NIDAL HADROU <nidal@DESKTOP-F2CC40J.com>
2022-11-29 11:20:48 +0100

    change procfile

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-from typing import Optional
-from fastapi import FastAPI, HTTPException, Depends, Request
-from fastapi.responses import JSONResponse
-from fastapi_jwt_auth import AuthJWT
-from fastapi_jwt_auth.exceptions import AuthJWTException
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import psycopg2
 from typing import List
+
 import databases
+
 import sqlalchemy
 
-DATABASE_URL = "postgresql://suhcsqekypfson:399b493c1c5e6d81c81833d4f33830b16d5fc671e7e4127a1a61a244c4702706@ec2-44-205-112-253.compute-1.amazonaws.com:5432/deopdbh5407fba"
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+DATABASE_URL = "postgresql://xnfajmqycpswkk:cb5b9e5b905cc9fd2b5605233b11cf6084f0b79a01e1df8869be80d70d08c4e9@ec2-44-195-100-240.compute-1.amazonaws.com:5432/dcig9h8hsmn9if"
 
 database = databases.Database(DATABASE_URL)
 
+
 metadata = sqlalchemy.MetaData()
 
-users = sqlalchemy.Table(
 
-    "users",
+
+notes = sqlalchemy.Table(
+
+    "notes",
 
     metadata,
 
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
 
-    sqlalchemy.Column("username", sqlalchemy.String),
+    sqlalchemy.Column("text", sqlalchemy.String),
 
-    sqlalchemy.Column("password", sqlalchemy.String),
-
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
 
 )
 
@@ -38,12 +38,20 @@ engine = sqlalchemy.create_engine(
 )
 metadata.create_all(engine)
 
-class Usere(BaseModel):
-    id: int = None
-    username: str
-    password: str
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
 
 app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup():
@@ -53,15 +61,17 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-@app.get("/", response_model=List[Usere])
-async def read_users():
-    query = users.select()
+
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
     return await database.fetch_all(query)
 
-@app.post("/login", response_model=Usere)
-async def create_user(usere: Usere, Authorize: AuthJWT = Depends()):
-    query = users.insert().values(username=usere.username, password=usere.password)
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
     last_record_id = await database.execute(query)
-    return {**usere.dict(), "id": last_record_id}
-    
+    return {**note.dict(), "id": last_record_id}
 
